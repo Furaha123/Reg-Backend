12f3b54716a64f9d955d89675ff846144ee9c9c1
Furaha123 <furahamoses41@gmail.com>
2024-04-22 11:52:20 +0200

    Added database mongodb

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
